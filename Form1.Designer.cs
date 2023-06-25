94b35f97ca59ef56add3e0504da9dada9d2ccfd6
jackfuro <101521167+jackfuro@users.noreply.github.com>
2023-06-25 21:08:34 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Wypełnianie_obszaru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Wypełnianie_obszaru.Properties.Resources.Nowy_obraz_mapy_bitowej;
+            this.pictureBox1.Location = new System.Drawing.Point(103, 90);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(304, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Wypełnianie_obszaru.Properties.Resources.Nowy_obraz_mapy_bitowej;
+            this.pictureBox2.Location = new System.Drawing.Point(337, 107);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Wypełnianie_obszaru
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            FillArea(5, 5);
+        }
+        private void FillArea(int x, int y )
+        {
+            Bitmap image = (Bitmap)pictureBox1.Image;
+            int width = image.Width;
+            int height = image.Height;
+            Color borderColor = image.GetPixel(1, 1);
+            Color fillColor = Color.Azure;
+            // Sprawdzanie, czy punkt znajduje się w obszarze obrazka
+            if (x < 0 || x >= width || y < 0 || y >= height)
+            {
+                return;
+            }
+
+
+            // Sprawdzanie, czy piksel ma odpowiedni kolor
+            //Color pixelColor = image.GetPixel(x, y);
+            //if (pixelColor != borderColor && pixelColor != insideColor)
+            //{
+            //    return;
+            //}
+
+            // Wypełnianie piksela nowym kolorem
+            image.SetPixel(x, y, fillColor);
+
+            // Rekurencyjne wypełnianie sąsiadujących pikseli
+            //if (image.GetPixel(x - 1, y) != borderColor && image.GetPixel(x-1,y)!=fillColor)
+            //{ FillArea(x - 1, y); }
+            //if (image.GetPixel(x + 1, y) != borderColor && image.GetPixel(x + 1, y) != fillColor)
+            //{ FillArea(x + 1, y); }
+            //if (image.GetPixel(x, y - 1) != borderColor && image.GetPixel(x , y-1) != fillColor)
+            //{ FillArea(x, y - 1); }
+            //if (image.GetPixel(x, y + 1) != borderColor && image.GetPixel(x, y+1) != fillColor)
+            //{ FillArea(x, y + 1); }
+            pictureBox1.Image = image;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap image = (Bitmap)pictureBox1.Image;
+            image.SetPixel(5, 5, Color.Azure);
+            image.SetPixel(6, 5, Color.Azure);
+            image.SetPixel(7, 5, Color.Azure);
+            pictureBox2.Image = image;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
